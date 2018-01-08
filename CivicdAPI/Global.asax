c8b812fe9de2d4adf9c7d4047fb93c7358aa75d1
Eleanor Palmer <ePalmer@hhogdev.com>
2018-01-08 09:16:04 -0500

    Add project files.

---
+++ CivicdAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CivicdAPI.MvcApplication" Language="C#" %>
---
+++ CivicdAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CivicdAPI.Startup))]
+namespace CivicdAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
