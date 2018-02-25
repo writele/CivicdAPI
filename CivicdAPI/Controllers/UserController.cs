8afa5041ee54b09040949569022a67b703d5d835
Eleanor Palmer <ePalmer@hhogdev.com>
2018-02-25 16:43:34 -0500

    Added user controller with register call. Moved user profile request call.

---
+++ CivicdAPI/Controllers/UserController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using CivicdAPI.Models;
+using CivicdAPI.Providers;
+using CivicdAPI.Results;
+
+namespace CivicdAPI.Controllers
+{
+  [Authorize]
+  [RoutePrefix("api")]
+  public class UserController : ApiController
+  {
+    private const string LocalLoginProvider = "Local";
+    private ApplicationUserManager _userManager;
+    private ApplicationDbContext db = new ApplicationDbContext();
+
+    public UserController()
+    {
+    }
+
+    public UserController(ApplicationUserManager userManager,
+      ISecureDataFormat<AuthenticationTicket> accessTokenFormat)
+    {
+      UserManager = userManager;
+      //AccessTokenFormat = accessTokenFormat;
+    }
+    public ApplicationUserManager UserManager
+    {
+      get
+      {
+        return _userManager ?? Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+      }
+      private set
+      {
+        _userManager = value;
+      }
+    }
+
+    // POST api/Users
+    [AllowAnonymous]
+    [Route("Users")]
+    [HttpPost]
+    public async Task<IHttpActionResult> Register(UserViewModel model)
+    {
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      var address = new Address()
+      {
+        StreetAddressOne = model.StreetAddressOne,
+        StreetAddressTwo = model.StreetAddressTwo,
+        City = model.City,
+        State = model.State,
+        ZipCode = model.ZipCode
+      };
+
+      var tags = new List<Tag>();
+
+      foreach (var item in model.Tags)
+      {
+        var tag = db.Tags.FirstOrDefault(x => x.Name == item.Name);
+        tags.Add(tag);
+      }
+
+      var user = new ApplicationUser()
+      {
+        UserName = model.Email,
+        Email = model.Email,
+        FirstName = model.FirstName,
+        LastName = model.LastName,
+        DisplayName = model.DisplayName,
+        ProfileDescription = model.ProfileDescription,
+        Address = address,
+        Tags = tags
+      };
+
+      IdentityResult result = await UserManager.CreateAsync(user, model.Password.ToString());
+
+      if (!result.Succeeded)
+      {
+        return GetErrorResult(result);
+      }
+
+      return Ok();
+    }
+
+    [HttpGet]
+    [Route("Users/{userEmail}")]
+    public async Task<UserViewModel> GetUserByEmail(string userEmail)
+    {
+
+      var matchedUser = await UserManager.FindByEmailAsync(userEmail).ConfigureAwait(false);
+      if (matchedUser == null)
+      {
+        //TODO: Specific exception message maybe?
+        throw new Exception("Unable to Find Matching User");
+      }
+
+      return new UserViewModel
+      {
+        Email = matchedUser.Email,
+        DisplayName = matchedUser.DisplayName,
+        FirstName = matchedUser.FirstName,
+        LastName = matchedUser.LastName,
+        ProfileDescription = matchedUser.ProfileDescription,
+        StreetAddressOne = matchedUser.Address?.StreetAddressOne,
+        StreetAddressTwo = matchedUser.Address?.StreetAddressTwo,
+        City = matchedUser.Address?.City,
+        State = matchedUser.Address?.State,
+        ZipCode = matchedUser.Address?.ZipCode,
+        Tags = matchedUser.Tags?.Select(t => new Models.DTO.TagDTO
+        {
+          Id = t.ID,
+          Name = t.Name
+        })
+      };
+    }
+
+    private IHttpActionResult GetErrorResult(IdentityResult result)
+    {
+      if (result == null)
+      {
+        return InternalServerError();
+      }
+
+      if (!result.Succeeded)
+      {
+        if (result.Errors != null)
+        {
+          foreach (string error in result.Errors)
+          {
+            ModelState.AddModelError("", error);
+          }
+        }
+
+        if (ModelState.IsValid)
+        {
+          // No ModelState errors are available to send, so just return an empty BadRequest.
+          return BadRequest();
+        }
+
+        return BadRequest(ModelState);
+      }
+
+      return null;
+    }
+  }
+}
---
+++ CivicdAPI/Models/DTO/UserViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CivicdAPI.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security;
+using System.Web;
+
+namespace CivicdAPI.Models
+{
+  public class UserViewModel
+  {
+    [Required]
+    public string Email { get; set; }
+    [Required]
+    public string Password { get; set; }
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+    public string DisplayName { get; set; }
+    public string ProfileDescription { get; set; }
+    public string StreetAddressOne { get; set; }
+    public string StreetAddressTwo { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string ZipCode { get; set; }
+
+    public IEnumerable<TagDTO> Tags { get; set; }
+  }
+}
