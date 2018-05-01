f5129d71c9afbf32d0e323b585c161f851efdae7
Russell Dusch <rcd11039@gmail.com>
2018-04-30 22:02:21 -0400

    Formatted and removed unused usings for Activity.cs

--- CivicdAPI/Models/Activity.cs
+++ CivicdAPI/Models/Activity.cs
@@ -1,30 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace CivicdAPI.Models
 {
-  public class Activity
-  {
-    public Activity()
+    public class Activity
     {
-      this.Tags = new HashSet<Tag>();
+        public Activity()
+        {
+            this.Tags = new HashSet<Tag>();
+        }
+        [Key]
+        public int ID { get; set; }
+        public string DisplayTitle { get; set; }
+        [AllowHtml]
+        public string Description { get; set; }
+        public string Photo { get; set; }
+        public DateTimeOffset StartTime { get; set; }
+        public DateTimeOffset? EndTime { get; set; }
+        public ActivityCategory Category { get; set; }
+        public Address Address { get; set; }
+        public virtual ICollection<UserActivity> UserActivities { get; set; }
+        public virtual ICollection<Tag> Tags { get; set; }
     }
-    [Key]
-    public int ID { get; set; }
-    public string DisplayTitle { get; set; }
-    [AllowHtml]
-    public string Description { get; set; }
-    public string Photo { get; set; }
-    public DateTimeOffset StartTime { get; set; }
-    public DateTimeOffset? EndTime { get; set; }
-    public ActivityCategory Category { get; set; }
-    public Address Address { get; set; }
-    public virtual ICollection<UserActivity> UserActivities { get; set; }
-    public virtual ICollection<Tag> Tags { get; set; }
-  }
 }
